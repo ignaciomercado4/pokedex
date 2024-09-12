2d5038bd24a3ff97c1aa1b9745076336a99349d7
Ignacio Mercado <igmercado46@gmail.com>
2024-09-11 23:44:10 -0400

    finishes E2E testing

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -19,10 +19,33 @@ describe('pokédex', () => {
     cy.get('#pokemon-container').should('exist');
     cy.wait(600);
     cy.get('.pokemon-card').first().should('exist');
-    cy.get('.pokemon-name-and-id').first().should('have.text', 'Bulbasaur #1');
-    cy.get('.pokemon-height').eq(0).should('have.text', '0.7 mts / 2ft 4in');
-    cy.get('.pokemon-weight').eq(0).should('have.text', '6.9 kg / 15.2 lbs');
-    cy.get('.pokemon-type').eq(0).should('have.text', 'grass, poison');
 
-  })
-})
+    cy.get('.pokemon-name-and-id').first().invoke('text').then(text => {
+      expect(text.trim()).to.equal('Bulbasaur #1');
+    });
+
+    cy.get('.pokemon-height').first().invoke('text').then(text => {
+      expect(text.trim()).to.equal('Height: 0.7 mts / 2ft 4in');
+    });
+
+    cy.get('.pokemon-weight').first().invoke('text').then(text => {
+      expect(text.trim()).to.equal('Weight: 6.9 kg / 15.2 lbs');
+    });
+
+    cy.get('.pokemon-type').first().invoke('text').then(text => {
+      expect(text.trim()).to.equal('Type(s): grass, poison');
+    });
+  });
+
+  it('should display 20 more pokémon cards after scrolling to the bottom', () => {
+    cy.wait(600);
+    cy.get('.pokemon-card').should('exist').and('have.length.above', 19);
+    cy.scrollTo('bottom');
+    cy.get('#loading-message').invoke('text').then(text => {
+      expect(text.trim()).to.equal('Loading...');
+    });
+    cy.wait(600);
+    cy.get('.pokemon-card').should('exist').and('have.length.above', 39);
+
+  });
+});
